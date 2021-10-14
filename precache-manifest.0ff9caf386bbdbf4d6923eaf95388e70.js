235cd81c59960f86036050a3403b215fefc25ed1
Cássio Pereira <cassiorodp@gmail.com>
2021-10-14 10:46:36 -0300

    Updates

---
+++ precache-manifest.0ff9caf386bbdbf4d6923eaf95388e70.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7525e1f17bba8d4f3a4ff0b49b51f496",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "a519fd4704be73b5ee22",
+    "url": "/recipe-app/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "3b0b728c401d2c9d2e12",
+    "url": "/recipe-app/static/css/main.a1d33336.chunk.css"
+  },
+  {
+    "revision": "a519fd4704be73b5ee22",
+    "url": "/recipe-app/static/js/2.596da2a0.chunk.js"
+  },
+  {
+    "revision": "0cff4be5140e6d649e874aa2233a529e",
+    "url": "/recipe-app/static/js/2.596da2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0b728c401d2c9d2e12",
+    "url": "/recipe-app/static/js/main.758583b2.chunk.js"
+  },
+  {
+    "revision": "4b9365336cfc937175bb",
+    "url": "/recipe-app/static/js/runtime-main.b95f7359.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipe-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "5ec8e291b9a341f387548ffcaac6aeca",
+    "url": "/recipe-app/static/media/logo.5ec8e291.png"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipe-app/static/media/shareIcon.06215ed4.svg"
+  }
+]);
