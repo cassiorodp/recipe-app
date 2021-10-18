e6c14d7049fbe5e9fd4676dc774a2a976368c1c2
Cássio Pereira <cassiorodp@gmail.com>
2021-10-18 17:51:46 -0300

    add final styles

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
 import App from '../App';
+import { Profile } from '../pages';
 
 const PROFILE_EMAIL = 'profile-email';
 const DONE_BTN = 'profile-done-btn';
@@ -45,6 +46,7 @@ describe('1. Testar o componente profile', () => {
       clear: jest.fn(),
     };
     global.localStorage = localStorageMock;
+
     const logoutBtn = screen.getByTestId(LOGOUT_BTN);
     userEvent.click(logoutBtn);
 
@@ -62,5 +64,25 @@ describe('1. Testar o componente profile', () => {
     userEvent.click(doneBtn);
 
     expect(mockHistory.location.pathname).toBe('/receitas-feitas');
+  });
+});
+describe('Testa funcionalidades do Profile', () => {
+  beforeEach(() => {
+    const { history } = renderWithReduxAndRouter(<Profile />);
+    mockHistory = history;
+  });
+  it('1-3 Testa se, ao clicar em Receitas feitas, redireciona à lista.', () => {
+    const doneBtn = screen.getByTestId(DONE_BTN);
+
+    act(() => { userEvent.click(doneBtn); });
+
+    expect(mockHistory.location.pathname).toBe('/receitas-feitas');
+  });
+  it('1-4 Testa se, ao clicar em Receitas Favoritas, redireciona à lista', () => {
+    const favoriteButton = screen.getByRole('button', { name: /receitas favoritas/i });
+
+    act(() => { userEvent.click(favoriteButton); });
+
+    expect(mockHistory.location.pathname).toBe('/receitas-favoritas');
   });
 });
--- src/tests/Progress.test.js
+++ src/tests/Progress.test.js
@@ -9,6 +9,8 @@ import App from '../App';
 
 // Helpers
 import renderWithReduxAndRouter from '../helpers/renderWithReduxAndRouter';
+// Mock copy library
+jest.mock('clipboard-copy');
 
 // Variables;
 const RECIPE_PHOTO = 'recipe-photo';
@@ -23,10 +25,14 @@ const FINISH_RECIPE_BTN = 'finish-recipe-btn';
 // Mock copy library
 jest.mock('clipboard-copy');
 
-describe('Testa a página de login', () => {
+// History mock
+let historyMock;
+
+describe('Testa a página de progresso', () => {
   beforeEach(() => {
-    renderWithReduxAndRouter(<App />,
+    const { history } = renderWithReduxAndRouter(<App />,
       { initialState: {}, initialEntries: ['/comidas/52977/in-progress'] });
+    historyMock = history;
   });
 
   it('Contem os elementos da tela', async () => {
@@ -61,10 +67,14 @@ describe('Testa a página de login', () => {
   it('testa botão de favorito', async () => {
     const favoriteBtn = await screen.findByTestId(FAVORITE_BTN);
     const fullHeart = 'http://localhost/blackHeartIcon.svg';
+    const emptyHeart = 'http://localhost/whiteHeartIcon.svg';
     userEvent.click(favoriteBtn);
 
     expect(favoriteBtn).toBeInTheDocument();
     expect(fullHeart).toEqual(favoriteBtn.src);
+
+    userEvent.click(favoriteBtn);
+    expect(emptyHeart).toEqual(favoriteBtn.src);
   });
 
   it('testa botão de compartilhar', async () => {
@@ -78,12 +88,36 @@ describe('Testa a página de login', () => {
     expect(shareMsg).toBeInTheDocument();
   });
 
-  // it('testa as checkboxs e o botao de finalizar receita', async () => {
-  //   const checkboxes = await screen.findAllByTestId(/ingredient-step/i);
-  //   checkboxes.forEach((checkbox) => {
-  //     userEvent.click(checkbox);
-  //   });
-  //   const finishBtn = await screen.findByTestId(FINISH_RECIPE_BTN);
-  //   expect(finishBtn.disabled).toBeFalsy();
-  // });
+  it('testa as checkboxs e o botao de finalizar receita', async () => {
+    const checkboxes = await screen.findAllByTestId(/ingredient-step/i);
+
+    checkboxes.forEach((checkbox) => {
+      userEvent.click(checkbox);
+    });
+
+    const finishBtn = await screen.findByTestId(FINISH_RECIPE_BTN);
+    expect(finishBtn.disabled).toBeFalsy();
+
+    userEvent.click(finishBtn);
+
+    expect(historyMock.location.pathname).toBe('/receitas-feitas');
+  });
+
+  it('testa funcionamento de atualizar data da receita', async () => {
+    const recipe = [{ id: '52977', type: 'comida', area: 'Turkish', category: 'Side', alcoholicOrNot: '', name: 'Corba', image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg', doneDate: '02/10/2021', tags: ['Soup'] }];
+
+    localStorage.setItem('doneRecipes', JSON.stringify(recipe));
+
+    const checkboxes = await screen.findAllByTestId(/ingredient-step/i);
+
+    checkboxes.forEach((checkbox) => {
+      userEvent.click(checkbox);
+    });
+
+    const finishBtn = await screen.findByTestId(FINISH_RECIPE_BTN);
+    userEvent.click(finishBtn);
+
+    const date = JSON.parse(localStorage.getItem('doneRecipes'))[0].doneDate;
+    expect(date).not.toBe(recipe[0].doneDate);
+  });
 });
